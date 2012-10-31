376f8f2d370d1fdb4a25e554d127807d3fc28624
Taylor Trimble <t.trim@me.com>
2012-10-31 14:59:27 -0500

    Add vector functions skeleton

---
+++ MiniVec.c
@@ -0,0 +1,14 @@
+/* MiniVec - Really, really small library for performing uniform math on arrays
+ * Organization: Tesla Works
+ * Project: Animatronic heads
+ */
+
+void MVAdd(char vector[], int vectorCount, int numberToAdd)
+{
+
+}
+
+void MVRightShift(char vector[], int vectorCount, int bitsToShift)
+{
+    
+}
---
+++ MiniVec.h
@@ -0,0 +1,7 @@
+/* MiniVec - Really, really small library for performing uniform math on arrays
+ * Organization: Tesla Works
+ * Project: Animatronic heads
+ */
+
+void MVAdd(char vector[], int vectorCount, int numberToAdd);
+void MVRightShift(char vector[], int vectorCount, int bitsToShift);
