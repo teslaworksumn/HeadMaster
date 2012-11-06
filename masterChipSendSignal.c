1192d39b81b59be716e07ec7ee17d1e3427dd46b
Kevan Ahlquist <ahlqu039@umn.edu>
2012-11-06 11:25:11 -0600

    Update to use DMXlib.h
    Ripped out internal functions

--- masterChipSendSignal.c
+++ masterChipSendSignal.c
@@ -10,6 +10,7 @@
 */
 
 #include <p18f2620.h>
+#include DMXlib.h
 
 //config from dmx code ******************************************************
 /*
@@ -72,134 +73,9 @@ void low_isr(void)
 {
 }
 //*****************************************************************************
-char RxBuffer[512];
-int numReceivedBytes; //16-bit counter
-char CountL, CountH; //16-bit counter (OLD, try to remove)
-char inputBuffer; //used to replace W in ASM code, may just be able to use COUNTL to save variables
 char slaveAddress[4]; //TODO: Need to hardcode slave addresses in this array
 const int BYTES_PER_SLAVE = 8; //TODO: Change to number of DMX channels used per slave
-const int DMXStartCode = 0x00;
 //******************************************************************************
-void setupDMX(void)
-{
-    OSCTUNEbits.PLLEN = 1; //Set PLL on. Not sure why this isn't in the main setup function
-    for (int i = 0; i < 512; i++) { //Clear the receive buffer 
-    	RxBuffer[i] = 0;
-    }
-
-	TRISCbits.TRISC7 = 1;	//Allow the EUSART RX to control pin RC7
-	TRISCbits.TRISC6 = 1;	//Allow the EUSART RX to control pin RC6
-
-    BAUDCONbits.BRG16 = 1; 	//Enable EUSART for 16-bit Asynchronous operation
-	SPBRGH = 0;
-	
-    SPBRG = .31; 			//Baud rate is 250KHz for 32MHz Osc. freq.
-
-    TXSTA = 0x04;			//Enable transmission and CLEAR high baud rate
-
-    RCSTA = 0x90;			//Enable serial port and reception
-}
-
-void receiveDMX(void)
-{
-//_asm
-MainLoop:
-//    bsf     PORTB,RB0
-//    bsf     PORTB,RB1
-    PORTBbits.RB0 = 1;
-    PORTBbits.RB1 = 1;
-    
-//; First loop, synchronizing with the transmitter
-
-//WaitBreak
-//    btfsc   RCSTA,FERR
-//    bra     GotBreak
-//    btfss   RCSTA,OERR
-//    bra     WaitBreak
-//    bcf     RCSTA,CREN
-//    bsf     RCSTA,CREN
-	while (1) {
-    	while (!RCSTAbits.FERR) {
-        	if (RCSTAbits.OERR) {
-            	RCSTAbits.CREN = 0;		//Toggle CREN to clear OERR flag
-            	RCSTAbits.CREN = 1;
-            	break;
-        	}
-    	}
-    }
-
-//GotBreak:
-//    movf    RCREG,W                 ;Read the Receive buffer to clear the error condition
-    inputBuffer = RCREG; 
-
-//;Second loop, waiting for the START code
-//WaitForStart:
-//    btfss   PIR1,RCIF               ;Wait until a byte is correctly received
-//    bra     WaitForStart
-//    btfsc   RCSTA,FERR              ;Got a byte
-//    bra     GotBreak
-//    movf    RCREG,W
-
-	while (1) {
-		while (!PIR1bits.RCIF) ;	//Wait until a byte is correctly received
-		if (RCSTAbits.FERR) {		//Framing error
-			inputBuffer = RCREG;
-		} else {					//Got a byte
-			inputBuffer = RCREG;
-			break;
-		}
-	}
-
-//; Check for the START code value, if it is not 0, ignore the rest of the frame
-//    andlw   0xff
-//    bnz     MainLoop                ;Ignore the rest of the frame if not zero 
-    if (inputBuffer != DMXStartCode) { //if current byte isn't START code, ignore the frame
-        goto MainLoop;
-    }
-  
-//; Init receive counter and buffer pointer        
-//    clrf    CountL
-//    clrf    CountH
-//    lfsr    FSR2,RxBuffer
-    numReceivedBytes = 0;	//initialize counter
-
-//; Third loop, receiving 512 bytes of data
-WaitForData:	//considering pulling this out as a function to avoid goto statement
-//    btfsc   RCSTA,FERR          ;If a new framing error is detected (error or short frame)
-//    bra     MainLoop            ; the rest of the frame is ignored and a new synchronization
-//                                ; is attempted
-//    btfss   PIR1,RCIF           ;Wait until a byte is correctly received
-//    bra     WaitForData
-//    movf    RCREG,W
-
-    while (1) {
-    	if (RCSTAbits.FERR) {	//If a new framing error is detected (error or short frame)
-        	goto MainLoop;		// the rest of the frame is ignored and a new synchronization
-    	}						//is attempted
-    	if (PIR1bits.RCIF) {	//Wait until a byte is correctly received
-        	break;
-    	}
-    }
-    inputBuffer = RCREG;		//read received byte from RCREG
-	
-//MoveData
-//    movwf   POSTINC2            ;Move the received data to the buffer 
-//                                ; (auto-incrementing pointer)
-//    incf    CountL,F            ;Increment 16-bit counter
-//    btfss   STATUS,C
-//    bra     WaitForData
-//    incf    CountH,F
-    RxBuffer[numReceivedBytes++] = inputBuffer;
-
-//    btfss   CountH,1            ;Check if 512 bytes of data received
-//    bra     WaitForData
-//    return
-    if (numReceivedBytes < 512) {
-        goto WaitForData;
-    }
-    return;
-//_endasm
-}
 
 /**
  * Send BYTES_PER_SLAVE bytes of data from the buffer via I2C to the indicated receiver. 
@@ -265,10 +141,10 @@ void main(void)
     int receiver = 0;
 
     setup();
-    setupDMX();
+    DMXSetup();
     while(1)
     {
-
+		DMXReceive();
         for (receiver = 0; receiver < 4; ++receiver)
 
         {
