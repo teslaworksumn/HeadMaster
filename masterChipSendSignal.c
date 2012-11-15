302590333467b28bed106118a559541beeda43e2
CrasherCourse <micha454@umn.edu>
2012-11-15 00:30:33 -0600

    added remaining pragmas

--- masterChipSendSignal.c
+++ masterChipSendSignal.c
@@ -15,32 +15,40 @@
 // Device Configuration
 
 
-#pragma config OSC = HSPLL		//high speed/ PLL enabled oscilator mode
-#pragma config FCMEN = OFF		//fail-safe clock moniter
-#pragma config IESO = OFF		//inter/ext osc switchover (disabled)
-#pragma config BOREN = OFF		//brown-out reset bits
-#pragma config BORV = 3			//brown out voltage bit (min setting?)
-#pragma config PWRT = OFF		
-#pragma config WDT = OFF		//watch dog timer (off)
-#pragma config WDTPS = 32768	//wdt post-scaler (don't care)
-#pragma config CCP2MX = PORTC	//CCP2 mux bit: either PORTC or PORTBE
-#pragma config PBADEN = OFF
-#pragma config LPT1OSC = OFF
-#pragma config MCLRE = ON		//MCLR pin enabled
-#pragma config DEBUG = OFF
-#pragma config STVREN = ON		//stack full/underflow will cause reset when ON
-#pragma config LVP = OFF
-#pragma config XINST = OFF
+#pragma config OSC = HSPLL		// High speed/ PLL enabled oscilator mode
+#pragma config FCMEN = OFF		// Fail-safe clock moniter
+#pragma config IESO = OFF		// Int/ext osc switchover (disabled)
+#pragma config BOREN = OFF		// Brown-out reset bits disabled
+#pragma config BORV = 3			// Brown out voltage bit (min setting)
+#pragma config PWRT = OFF		// Power-up timer
+#pragma config WDT = OFF		// Watch dog timer (off)
+#pragma config WDTPS = 32768	// WDT post-scaler (don't care)
+#pragma config CCP2MX = PORTC	// CCP2 mux bit: either PORTC or PORTBE
+#pragma config PBADEN = OFF		// PortB A/D Enable bit
+#pragma config LPT1OSC = OFF	// Low-Power Timer1 Oscillator
+#pragma config MCLRE = ON		// MCLR pin enabled
+#pragma config DEBUG = OFF		// Background Debugger Enable bit
+#pragma config STVREN = ON		// Stack full/underflow will cause reset when ON
+#pragma config LVP = OFF		// Single-supply ICSP Enable bit
+#pragma config XINST = OFF		// Extended Instruction Set Enable bit
 
-#pragma config CP0 = OFF		//all write protection bits, some missing, should be OFF
+#pragma config CP0 = OFF		// All write protection bits, should be OFF
 #pragma config CP1 = OFF
+#pragma config CP2 = OFF
+#pragma config CP3 = OFF
+#pragma config CPD = OFF
 #pragma config CPB = OFF
 #pragma config WRT0 = OFF
 #pragma config WRT1 = OFF
+#pragma config WRT2 = OFF
+#pragma config WRT3 = OFF
 #pragma config WRTB = OFF
 #pragma config WRTC = OFF
+#pragma config WRTD = OFF
 #pragma config EBTR0 = OFF
 #pragma config EBTR1 = OFF
+#pragma config EBTR2 = OFF
+#pragma config EBTR3 = OFF
 #pragma config EBTRB = OFF
 
 
