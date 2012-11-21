e1570ce467b36e9dadc5bb00164a061ed34dc9ce
Kevan Ahlquist <ahlqu039@umn.edu>
2012-11-21 14:32:46 -0600

    Magic number twiddling

--- I2C.h
+++ I2C.h
@@ -25,7 +25,11 @@
 // 
 
 // Constants
-const int BYTES_PER_SLAVE = 8;
+#define NUMBER_OF_SLAVES 4
+#define BYTES_PER_SLAVE 8
+
+char buffer[NUMBER_OF_SLAVES * BYTES_PER_SLAVE] = {7}; //Assuming the DMX parsing code will output data to this array
+char slaveAddresses[NUMBER_OF_SLAVES] = {40}; //TODO: Need to hardcode slave addresses in this array
 
 // Sends BYTES_PER_SLAVE bytes of data from the buffer via I2C to the receiver.
 // Reciever 0: bytes 0 to BYTES_PER_SLAVE
