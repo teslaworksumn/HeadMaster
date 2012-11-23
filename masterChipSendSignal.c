509d66805aa956b7c058901e8ba4ae3c3e13524e
Taylor Trimble <t.trim@me.com>
2012-11-23 13:09:37 -0600

    Use smaller calculation set

--- masterChipSendSignal.c
+++ masterChipSendSignal.c
@@ -144,7 +144,7 @@ void main(void)
     while(1)
     {
 		DMXReceive();
-		mapDmxToServo(DMXBuffer, DMX_BUFFER_SIZE);
+		mapDmxToServo(DMXBuffer + DMXStartChannel, NUMBER_OF_SLAVES * BYTES_PER_SLAVE);
         for (receiver = 0; receiver < NUMBER_OF_SLAVES; ++receiver) {
             sendI2C(receiver);
         }
