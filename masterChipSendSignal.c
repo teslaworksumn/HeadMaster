f3e5199d4542675ba63403bbff59670b471f378f
CrasherCourse <micha454@umn.edu>
2012-11-27 10:55:06 -0600

    added protection bits for blocks 4 and 5

--- masterChipSendSignal.c
+++ masterChipSendSignal.c
@@ -36,12 +36,16 @@
 #pragma config CP1 = OFF
 #pragma config CP2 = OFF
 #pragma config CP3 = OFF
+#pragma config CP4 = OFF
+#pragma config CP5 = OFF
 #pragma config CPD = OFF
 #pragma config CPB = OFF
 #pragma config WRT0 = OFF
 #pragma config WRT1 = OFF
 #pragma config WRT2 = OFF
 #pragma config WRT3 = OFF
+#pragma config WRT4 = OFF
+#pragma config WRT5 = OFF
 #pragma config WRTB = OFF
 #pragma config WRTC = OFF
 #pragma config WRTD = OFF
@@ -49,6 +53,8 @@
 #pragma config EBTR1 = OFF
 #pragma config EBTR2 = OFF
 #pragma config EBTR3 = OFF
+#pragma config EBTR4 = OFF
+#pragma config EBTR5 = OFF
 #pragma config EBTRB = OFF
 
 // Interrupts
