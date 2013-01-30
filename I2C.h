d0cd0da5d152f5f06467b7f7f80fe6f4aa180f67
Matthew Lewis <matt@mplewis.com>
2013-01-30 01:45:27 -0600

    cleared up changes requested by @tylrtrmbl in I2C.h

--- I2C.h
+++ I2C.h
@@ -18,17 +18,13 @@
 // =============================================================================
 
 char buffer[NUMBER_OF_SLAVES * BYTES_PER_SLAVE] = {7}; //Assuming the DMX parsing code will output data to this array
-char slaveAddresses[NUMBER_OF_SLAVES] = {40}; //TODO: Need to hardcode slave addresses in this array
+char slaveAddresses[NUMBER_OF_SLAVES] = {40};
 
 // =============================================================================
 // Functions
 // =============================================================================
 
-// Sends BYTES_PER_SLAVE bytes of data from the buffer via I2C to the receiver.
-// Reciever 0: bytes 0 to BYTES_PER_SLAVE
-// Reciever 1: bytes BYTES_PER_SLAVE to (2*BYTES_PER_SLAVE - 1)
-// etc.
+
 void I2CSend(int receiver);
 
-// Adds a single byte of data to the send buffer, does not wait for send completion to return
 void I2CSendByte(char data);
